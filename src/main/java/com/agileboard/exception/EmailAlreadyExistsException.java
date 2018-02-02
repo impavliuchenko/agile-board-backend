326a909c33ae18a2c14e957665c1420570c57911
Ivan Pavliuchenko <impavlyuchenko@gmail.com>
2018-02-02 15:26:05 +0200

    add security exceptions

---
+++ src/main/java/com/agileboard/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.agileboard.exception;
+
+public class EmailAlreadyExistsException extends Exception {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
